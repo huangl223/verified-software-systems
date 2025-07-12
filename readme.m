6482790059f07b8e6b1f0361fd26891b65777ce9
Li Huang <huangl223@mail2.sysu.edu.cn>
2025-07-12 16:35:35 +0200

    Create readme.m

---
+++ readme.m
@@ -0,0 +1,267 @@
+##Compiler
+CompCert
+Webpage: https://compcert.org/compcert-C.html
+
+Repository: https://github.com/AbsInt/CompCert 
+
+Kästner, Daniel, Jörg Barrho, Ulrich Wünsche, Marc Schlickling, Bernhard Schommer, Michael Schmidt, Christian Ferdinand, Xavier Leroy, and Sandrine Blazy. "CompCert: Practical experience on integrating and qualifying a formally verified optimizing compiler." In ERTS2 2018-9th European Congress Embedded Real-Time Software and Systems, pp. 1-9. 2018.
+
+Leroy, Xavier, Sandrine Blazy, Daniel Kästner, Bernhard Schommer, Markus Pister, and Christian Ferdinand. "CompCert-a formally verified optimizing compiler." In ERTS 2016: Embedded Real Time Software and Systems, 8th European Congress. 2016.
+
+Leroy, Xavier. "A formally verified compiler back-end." Journal of Automated Reasoning 43, no. 4 (2009): 363-446.
+
+
+##CakeML
+Webpage:  https://cakeml.org 
+
+Repository: https://github.com/CakeML/cakeml 
+
+Tan, Yong Kiam, Magnus O. Myreen, Ramana Kumar, Anthony Fox, Scott Owens, and Michael Norrish. "The verified CakeML compiler backend." Journal of Functional Programming 29 (2019).
+
+Kumar, Ramana, Magnus O. Myreen, Michael Norrish, and Scott Owens. "CakeML: a verified implementation of ML." ACM SIGPLAN Notices 49, no. 1 (2014): 179-191.
+
+Abrahamsson, Oskar, Son Ho, Hrutvik Kanabar, Ramana Kumar, Magnus O. Myreen, Michael Norrish, and Yong Kiam Tan. "Proof-producing synthesis of CakeML from monadic HOL functions." Journal of Automated Reasoning 64, no. 7 (2020): 1287-1306.
+
+Guéneau, Armaël, Magnus O. Myreen, Ramana Kumar, and Michael Norrish. "Verified characteristic formulae for CakeML." In European Symposium on Programming, pp. 584-610. Springer, Berlin, Heidelberg, 2017.
+
+
+Vellvm
+Webpage: https://www.cis.upenn.edu/~stevez/vellvm/ 
+
+Repository: https://github.com/vellvm/vellvm 
+
+Zhao, Jianzhou, Santosh Nagarakatte, Milo MK Martin, and Steve Zdancewic. "Formalizing the LLVM intermediate representation for verified program transformations." In Proceedings of the 39th annual ACM SIGPLAN-SIGACT symposium on Principles of programming languages, pp. 427-440. 2012.
+
+Zhao, Jianzhou, Santosh Nagarakatte, Milo MK Martin, and Steve Zdancewic. "Formal verification of SSA-based optimizations for LLVM." In Proceedings of the 34th ACM SIGPLAN conference on Programming language design and implementation, pp. 175-186. 2013.
+
+
+Lutsig
+Repository: https://github.com/CakeML/hardware   
+
+Lööw, Andreas. "Lutsig: a verified Verilog compiler for verified circuit development." In Proceedings of the 10th ACM SIGPLAN International Conference on Certified Programs and Proofs, pp. 46-60. 2021.
+
+Lööw, Andreas, and Magnus O. Myreen. "A proof-producing translator for verilog development in HOL." In 2019 IEEE/ACM 7th International Conference on Formal Methods in Software Engineering (FormaliSE), pp. 99-108. IEEE, 2019.
+
+
+VeriCert
+Webpage: https://vericert.ymhg.org/ 
+
+Repository: https://sr.ht/~ymherklotz/vericert/ 
+
+Yann Herklotz, James D. Pollard, Nadesh Ramanathan, and John Wickerson. Formal Verification of High-Level Synthesis. In Proc. ACM Program. Lang. 5, OOPSLA, 2021. 
+
+Yann Herklotz and John Wickerson. High-level synthesis tools should be proven correct. In Workshop on Languages, Tools, and Techniques for Accelerator Design, 2021.
+
+
+JITs
+Repository: https://github.com/uw-unsat/jitterbug   
+
+Presentation: https://www.usenix.org/conference/osdi20/presentation/nelson 
+
+Nelson, Luke, Jacob Van Geffen, Emina Torlak, and Xi Wang. "Specification and verification in the field: Applying formal methods to BPF just-in-time compilers in the Linux kernel." In 14th USENIX Symposium on Operating Systems Design and Implementation (OSDI), pp. 41-61. 2020.
+
+
+Jitk
+Repository: http://css.csail.mit.edu/jitk/ 
+
+Wang, Xi, David Lazar, Nickolai Zeldovich, Adam Chlipala, and Zachary Tatlock. "Jitk: A trustworthy in-kernel interpreter infrastructure." In 11th USENIX Symposium on Operating Systems Design and Implementation (OSDI 14), pp. 33-47. 2014.
+
+
+Vermilion
+Repository: https://github.com/slasser/vermillion 
+
+Lasser, Sam, Chris Casinghino, Kathleen Fisher, and Cody Roux. "A verified LL (1) parser generator." In 10th International Conference on Interactive Theorem Proving (ITP). Schloss Dagstuhl-Leibniz-Zentrum fuer Informatik, 2019.
+
+
+Q*cert
+Webpage: https://querycert.github.io/ (usable online)
+
+Braibant, Thomas, Jacques-Henri Jourdan, and David Monniaux. "Implementing and reasoning about hash-consed data structures in Coq." Journal of automated reasoning 53, no. 3 (2014): 271-304.
+
+Library
+EiffelBase2
+Repository: https://github.com/nadia-polikarpova/eiffelbase2/ 
+
+Polikarpova, Nadia, Julian Tschannen, and Carlo A. Furia. "A fully verified container library." In International Symposium on Formal Methods, pp. 414-434. Springer, Cham, 2015.
+
+
+HACL*
+Repository: https://github.com/project-everest/hacl-star 
+
+Zinzindohoué, Jean-Karim, Karthikeyan Bhargavan, Jonathan Protzenko, and Benjamin Beurdouche. "HACL*: A verified modern cryptographic library." In Proceedings of the ACM SIGSAC Conference on Computer and Communications Security, pp. 1789-1806. 2017.
+
+
+Amazon s2n
+Project description: https://galois.com/project/amazon-s2n/ 
+
+Repository: https://github.com/aws/s2n-tls 
+
+Chudnov, Andrey, Nathan Collins, Byron Cook, Joey Dodds, Brian Huffman, Colm MacCárthaigh, Stephen Magill et al. "Continuous formal verification of Amazon s2n." In International Conference on Computer Aided Verification, pp. 430-446. Springer, Cham, 2018.
+
+Beringer, Lennart, Adam Petcher, Q. Ye Katherine, and Andrew W. Appel. "Verified Correctness and Security of OpenSSL HMAC" In 24th USENIX Security Symposium (USENIX Security 15), pp. 207-221. 2015.
+
+Operating System
+seL4
+Webpage: https://sel4.systems/ 
+
+ Gernot Heiser: The seL4 microkernel - an introduction. Tech. rep., The seL4 Foundation (2020)
+
+Klein, G., Andronick, J., Elphinstone, K., Murray, T., Sewell, T., Kolanski, R., Heiser, G.: Comprehensive formal verification of an OS microkernel. Transactions on Computer Systems (TOCS) 32(1), 1–70 (2014)
+
+Sewell, T.A.L., Myreen, M.O., Klein, G.: Translation validation for a verified OS kernel. In: ACM SIGPLAN conference on programming language design and implementation (PLDI). pp. 471–482. ACM (2013)
+
+
+ProvenCore
+Webpage: https://provenrun.com/products/provencore/  
+
+Lescuyer, Stéphane. "ProvenCore: Towards a Verified Isolation Micro-Kernel." In MILS@ HiPEAC. 2015.
+
+
+mCertiKOS
+Project description: https://flint.cs.yale.edu/certikos/mcertikos.html#mcertikos 
+
+Gu, Ronghui, Jérémie Koenig, Tahina Ramananandro, Zhong Shao, Xiongnan Wu, Shu-Chun Weng, Haozhong Zhang, and Yu Guo. "Deep specifications and certified abstraction layers." ACM SIGPLAN Notices 50, no. 1 (2015): 595-608.
+
+ 
+
+mC2
+Project description: https://flint.cs.yale.edu/certikos/mc2.html#mc2 
+
+Gu, Ronghui, Zhong Shao, Hao Chen, Xiongnan Newman Wu, Jieung Kim, Vilhelm Sjöberg, and David Costanzo. "CertikOS: An extensible architecture for building certified concurrent OS kernels." In 12th USENIX Symposium on Operating Systems Design and Implementation (OSDI 16), pp. 653-669. 2016.
+
+
+Hyper-V
+Leinenbach, Dirk, and Thomas Santen. "Verifying the Microsoft Hyper-V hypervisor with VCC." In International Symposium on Formal Methods, pp. 806-809. Springer, Berlin, Heidelberg, 2009.
+
+
+PikeOS
+Webpage: https://www.sysgo.com/pikeos 
+
+Baumann, Christoph, Thorsten Bormer, Holger Blasum, and Sergey Tverdyshev. "Proving memory separation in a microkernel by code level verification." In 2011 14th IEEE International Symposium on Object/Component/Service-Oriented Real-Time Distributed Computing Workshops, pp. 25-32. IEEE, 2011.
+
+Beckert, Bernhard, Thorsten Bormer, Florian Merz, and Carsten Sinz. "Integration of bounded model checking and deductive verification." In International Conference on Formal Verification of Object-Oriented Software, pp. 86-104. Springer, Berlin, Heidelberg, 2011.
+
+
+ExpressOS
+Mai, Haohui, Edgar Pek, Hui Xue, Samuel Talmadge King, and Parthasarathy Madhusudan. "Verifying security invariants in ExpressOS." In Proceedings of the eighteenth international conference on Architectural support for programming languages and operating systems, pp. 293-304. 2013.
+
+
+SeKVM
+Li, Shih-Wei, Xupeng Li, Ronghui Gu, Jason Nieh, and John Zhuang Hui. "A Secure and Formally Verified Linux KVM Hypervisor." (2021).
+
+
+PROSPER
+Webpage: http://prosper.sics.se/index.html (cannot open)
+
+Dam, Mads, Roberto Guanciale, Narges Khakpour, Hamed Nemati, and Oliver Schwarz. "Formal verification of information flow security for a simple ARM-based separation kernel." In Proceedings of the 2013 ACM SIGSAC conference on Computer & communications security, pp. 223-234. 2013.
+
+
+ED Kernel
+Heitmeyer, Constance, Myla Archer, Elizabeth Leonard, and John McLean. "Applying formal methods to a certifiably secure software system." IEEE Transactions on Software Engineering 34, no. 1 (2008): 82-98.
+
+Heitmeyer, Constance L., Myla Archer, Elizabeth I. Leonard, and John McLean. "Formal specification and verification of data separation in a separation kernel for an embedded system." In Proceedings of the 13th ACM conference on Computer and communications security, pp. 346-355. 2006.
+
+
+MULTOS CA
+Hall, Anthony, and Roderick Chapman. "Correctness by construction: Developing a commercial secure system." IEEE software 19, no. 1 (2002): 18-25.
+
+
+Komodo
+Ferraiuolo, Andrew, Andrew Baumann, Chris Hawblitzel, and Bryan Parno. "Komodo: Using verification to disentangle secure-enclave hardware from software." In Proceedings of the 26th Symposium on Operating Systems Principles, pp. 287-305. 2017.
+
+Presentation slides - Project Komodo
+
+
+FSCQ
+Repository: https://github.com/mit-pdos/fscq 
+
+Chen, Haogang, Daniel Ziegler, Tej Chajed, Adam Chlipala, M. Frans Kaashoek, and Nickolai Zeldovich. "Using Crash Hoare logic for certifying the FSCQ file system." In Proceedings of the 25th Symposium on Operating Systems Principles, pp. 18-37. 2015.
+
+Chen, Haogang, Daniel Ziegler, Adam Chlipala, M. Frans Kaashoek, Eddie Kohler, and Nickolai Zeldovich. "Specifying crash safety for storage systems." In 15th Workshop on Hot Topics in Operating Systems (HotOS {XV}). 2015.
+
+SHOLIS
+King, Steve, Jonathan Hammond, Rod Chapman, and Andy Pryor. "Is proof more cost-effective than testing?." IEEE Transactions on software Engineering 26, no. 8 (2000): 675-686.
+
+Chapman, Roderick, and Florian Schanda. "Are we there yet? 20 years of industrial theorem proving with SPARK." In International Conference on Interactive Theorem Proving, pp. 17-26. Springer, Cham, 2014.
+
+King, Steve, Jonathan Hammond, Rod Chapman, and Andy Pryor. "The value of verification: Positive experience of industrial proof." In International Symposium on Formal Methods, pp. 1527-1545. Springer, Berlin, Heidelberg, 1999.
+
+White, Neil, Stuart Matthews, and Roderick Chapman. "Formal verification: will the seedling ever flower?." Philosophical Transactions of the Royal Society A: Mathematical, Physical and Engineering Sciences 375, no. 2104 (2017): 20150402.
+
+
+Lockheed C130J
+Croxford, Martin, and James Sutton. "Breaking through the V and V bottleneck." In International Eurospace-Ada-Europe Symposium, pp. 344-354. Springer, Berlin, Heidelberg, 1995.
+
+Boulanger, Jean-Louis, ed. Industrial use of formal methods: formal verification. John Wiley & Sons, 2013.
+
+Chapman, Roderick. "Industrial experience with SPARK." ACM SIGAda Ada Letters 20, no. 4 (2000): 64-68.
+
+
+NATS iFACTS
+White, Neil, Stuart Matthews, and Roderick Chapman. "Formal verification: will the seedling ever flower?." Philosophical Transactions of the Royal Society A: Mathematical, Physical and Engineering Sciences 375, no. 2104 (2017): 20150402.
+
+
+EuroFighter Typhoon
+ Adams, Mark M., and Philip B. Clayton. "ClawZ: Cost-effective formal verification for control systems." In International Conference on Formal Engineering Methods, pp. 465-479. Springer, Berlin, Heidelberg, 2005.
+
+O’Halloran, Colin. "Automated verification of code automatically generated from Simulink®." Automated Software Engineering 20, no. 2 (2013): 237-264.
+
+Database
+SWORD FEF
+Webpage:  http://www.lemma-one.com/ProofPower/fef/fef.html 
+
+FEF Project Final Report, 5 June 2016  
+
+
+Ynot
+Malecha, Gregory, Greg Morrisett, Avraham Shinnar, and Ryan Wisnesky. "Toward a verified relational database management system." In Proceedings of the 37th annual ACM SIGPLAN-SIGACT symposium on Principles of programming languages, pp. 237-248. 2010.
+
+Rail transport
+Roissy Shuttle
+Badeau, Frédéric, and Arnaud Amelot. "Using B as a high level programming language in an industrial project: Roissy VAL." In International Conference of B and Z Users, pp. 334-354. Springer, Berlin, Heidelberg, 2005.
+
+
+Dutch Tunnel Control
+Oortwijn, Wytse, and Marieke Huisman. "Formal verification of an industrial safety-critical traffic tunnel control system." In International Conference on Integrated Formal Methods, pp. 418-436. Springer, Cham, 2019.
+
+Nuclear Power
+Sizewell B
+Ward, N. J. "The rigorous retrospective static analysis of the Sizewell ‘B’Primary Protection System software." In International Conference on Computer Safety, Reliability and Security, pp. 171-181. Springer, London, 1993.
+
+Wall, N., and A. Kossilov. Verification and validation of software related to nuclear power plant control and instrumentation. No. IAEA--12-SP-384.37. 1994.
+
+Social internet application
+Ironclad Apps
+Hawblitzel, Chris, Jon Howell, Jacob R. Lorch, Arjun Narayan, Bryan Parno, Danfeng Zhang, and Brian Zill. "Ironclad apps: End-to-end security via automated full-system verification." In 11th USENIX Symposium on Operating Systems Design and Implementation (OSDI 14), pp. 165-181. 2014.
+
+Presentation slides -- Ironclad Apps: End-to-End Security via Automated Full-System Verification  
+
+
+Quark
+Webpage: http://goto.ucsd.edu/quark/ 
+
+Jang, Dongseok, Zachary Tatlock, and Sorin Lerner. "Establishing browser security guarantees through formal shim verification." In 21st USENIX Security Symposium (USENIX Security), pp. 113-128. 2012.
+
+Presentation at USENIX Security 2012
+
+Jang, Dongseok. Language-based Security for Web Browsers. University of California, San Diego, 2014.
+
+
+Cocon
+Project description: https://devel.isa-afp.org/entries/CoCon.html 
+
+Popescu, Andrei, Peter Lammich, and Ping Hou. "CoCon: A conference management system with formally verified document confidentiality." Journal of Automated Reasoning 65, no. 2 (2021): 321-356.
+
+
+Cosmed
+Webpage: https://www.andreipopescu.uk/CoSMed.html 
+
+Bauereiß, Thomas, Armando Pesenti Gritti, Andrei Popescu, and Franco Raimondi. "CoSMed: A confidentiality-verified social media platform." Journal of Automated Reasoning 61, no. 1 (2018): 113-139.
+
+Verification tools
+Flover
+Repository: https://gitlab.mpi-sws.org/AVA/FloVer 
+
+Bard, Joachim, Heiko Becker, and Eva Darulova. "Formally verified roundoff errors using SMT-based certificates and subdivisions." In International Symposium on Formal Methods, pp. 38-44. Springer, Cham, 2019.
+
+Becker, Heiko, Nikita Zyuzin, Raphaël Monat, Eva Darulova, Magnus O. Myreen, and Anthony Fox. "A verified certificate checker for finite-precision error bounds in Coq and HOL4." In 2018 Formal Methods in Computer Aided Design (FMCAD), pp. 1-10. IEEE, 2018.
